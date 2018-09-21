281c799c500cf6cd6b02a815e57416f2db3a5568
wsanjuan <wsanjuan@redlab.com.mx>
2018-09-21 16:46:55 -0500

    segundo commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace triangulo_de_pascal_con_unos_WojciechAdalberto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Longitud;
+            Longitud = 5;
+            ImprimeTriangulo(Longitud);
+        }
+
+        static void ImprimeTriangulo(int VLongitud)
+        {
+            for ( int i = 1 ; i <= VLongitud ; i++ )
+            {
+                for ( int j= VLongitud - i ; j>0 ; j-- )
+                    Console.Write(" ");
+                for ( int j=0 ; j < i ; j++ )
+                    Console.Write("1 ");
+                Console.WriteLine();
+            }
+            return;
+        }
+    }
+}
